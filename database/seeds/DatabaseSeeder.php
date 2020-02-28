614606f0c3c3f343abb0075ecc4eb512e62a8d5a
IvanBelchev <ivanbelchev123@gmail.com>
2020-02-27 23:51:28 -0500

    Update DatabaseSeeder.php
    update du databaseseeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,11 +14,16 @@ class DatabaseSeeder extends Seeder
         //$path = '/seeds/tp1.sql';
         //DB::unprepared(file_get_contents($path));
 
-        $sql = file_get_contents(database_path() . '/seeds/actors.sql');
-        $sql = file_get_contents(database_path() . '/seeds/films.sql');
-        $sql = file_get_contents(database_path() . '/seeds/actors_films.sql');
-        $sql = file_get_contents(database_path() . '/seeds/languages.sql');
+        //$sql = file_get_contents(database_path() . '/seeds/actors.sql');
+        //DB::statement($sql);
 
+        //$sql = file_get_contents(database_path() . '/seeds/films.sql');
+        //DB::statement($sql);
+
+        //$sql = file_get_contents(database_path() . '/seeds/actors_films.sql');
+        //DB::statement($sql);
+
+        $sql = file_get_contents(database_path() . '/seeds/languages.sql');
         DB::statement($sql);
     }
 }
